c620a5d3ba583ce9a0d06d08fa8b08ae7d2b4e0c
Niladri080 <nilmandal080@gmail.com>
2025-09-28 17:22:59 +0530

    let's see

--- server/index.js
+++ server/index.js
@@ -14,7 +14,6 @@ app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 app.use(
   cors({
     origin: "https://nexus-pro-three.vercel.app",
-    credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE","OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
